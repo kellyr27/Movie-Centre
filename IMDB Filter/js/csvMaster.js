a87d3abb51ddfdd2fdd3bd7387566e4e883336ec
kellyr27 <83458976+kellyr27@users.noreply.github.com>
2022-01-02 07:52:49 +0800

    CSV Converter
    Finished the basics of the CSV converter. Working.

---
+++ IMDB Filter/js/csvMaster.js
@@ -0,0 +1,116 @@
+// requiring path and fs modules
+const path = require('path')
+const fs = require('fs')
+
+const titles = ['Const_IMDB', 'Your Rating', 'Date Rated',  'Title', 'URL', 'Title Type', 'IMDb Rating', 'Runtime (mins)', 'Year', 'Genres', 'Num Votes', 'Release Date', 'Directors']
+let master_list = []
+
+
+// joining path of directory 
+const directoryPath = path.join(__dirname, '../uploads')
+// passsing directoryPath and callback function
+// fs.readdir(directoryPath, function (err, files) {
+//     //handling error
+//     if (err) {
+//         return console.log('Unable to scan directory: ' + err)
+//     } 
+//     // listing all files using forEach
+//     files.forEach(function (file) {
+//       // Do whatever you want to do with the file
+//       csvConverter(file)
+//     });
+// });
+
+// Node.js program to demonstrate the
+// fs.readdirSync() method
+  
+// Function to get current filenames
+// in directory
+filenames = fs.readdirSync(directoryPath);
+  
+console.log("\nCurrent directory filenames:");
+filenames.forEach(file => {
+  csvConverter(file)
+});
+
+console.log(master_list.length)
+
+
+function containsDuplicate(Const_IMDB, master_list) {
+  for (let i = 0; i < master_list.length; i++) {
+    if (master_list[i]['Const_IMDB'] === Const_IMDB) {
+      return true
+    }
+  }
+  return false
+}
+
+
+function csvConverter(file) {
+
+  const data = fs.readFileSync(`../uploads/${file}`, {encoding:'utf8', flag:'r'});
+
+  // Split data by line breaks into an array
+  let fileArray = data.split("\r\n")
+  let modifiedfileArray = []
+  // Split rows by commas
+  for (let row of fileArray) {
+    let rowArray = []
+    let current = ''
+    let multipleLock = false
+    for (let char of row+',') {
+      if (char === '"') {
+        multipleLock = !multipleLock
+      }
+      else if ((char === ',') && (!multipleLock)) {
+        rowArray.push(current)
+        current = ''
+      }
+      else {
+        current += char
+      }
+    }
+    modifiedfileArray.push(rowArray)
+  }
+
+  // Remove the all without IMDB_ID (removes headers and blank lines)
+  let filteredObjArray = []
+  for (let row of modifiedfileArray) {
+    if ((row[0]) && (row[0].slice(0,2) === 'tt')) {
+      // Convert to objects
+      let rowObj = {}
+      titles.forEach(function(val, index) {
+        rowObj[val] = row[index];
+      })
+
+      filteredObjArray.push(rowObj)
+    }
+    
+  }
+
+  for (let obj of filteredObjArray) {
+    // Convert "Your Rating", "IMDB Rating", "Runtime" and "Year" to number type
+    obj['Your Rating'] = parseInt(obj['Your Rating'])
+    obj['IMDb Rating'] = parseInt(obj['IMDb Rating'])
+    obj['Runtime (mins)'] = parseInt(obj['Runtime (mins)'])
+    obj['Year'] = parseInt(obj['Year'])
+
+    // Convert Genres and DIrectors to list type
+    obj['Genres'] = obj['Genres'].split(',')
+    for (let i = 0; i < obj['Genres'].length; i++) {
+      obj['Genres'][i] = obj['Genres'][i].trim()
+    }
+    obj['Directors'] = obj['Directors'].split(',')
+    for (let i = 0; i < obj['Directors'].length; i++) {
+      obj['Directors'][i] = obj['Directors'][i].trim()
+    }
+
+  }
+
+  // Remove duplicates
+  for (let obj of filteredObjArray) {
+    if (!containsDuplicate(obj['Const_IMDB'], master_list)) {
+      master_list.push(obj)
+    }
+  }
+}
